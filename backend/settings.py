7921a2f8ec6704122102c59180a1af77bfb65694
nap3831 <nap3831@gmail.com>
2021-04-13 21:36:37 +0700

    set env

--- backend/settings.py
+++ backend/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_URL = os.path.join(BASE_DIR, '/static/')
 # STATIC_URL = '/static/'
 FILEBROWSER_DIRECTORY = '../media/upload/'
 FILEBROWSER_DIRECTORY = ''
