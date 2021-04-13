ee45c2c1304f6fa302243fd85849b77a704d95c5
nap3831 <nap3831@gmail.com>
2021-04-13 21:54:33 +0700

    set env

--- backend/settings.py
+++ backend/settings.py
@@ -162,6 +162,6 @@ FILEBROWSER_DIRECTORY = ''
 MEDIA_URL = os.path.join(BASE_DIR, '/media/')
 # MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '/static/'),)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
