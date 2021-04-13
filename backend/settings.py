e62b71835b854dd5eec1bf68f1d5b043b8fe033d
nap3831 <nap3831@gmail.com>
2021-04-13 22:51:27 +0700

    set env

--- backend/settings.py
+++ backend/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
