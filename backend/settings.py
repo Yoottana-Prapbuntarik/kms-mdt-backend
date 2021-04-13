2c2915f037e7fbce7d3c1c3c32ca88748e1a23ba
nap3831 <nap3831@gmail.com>
2021-04-13 20:33:00 +0700

    set env

--- backend/settings.py
+++ backend/settings.py
@@ -95,9 +95,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(prod_db)
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
