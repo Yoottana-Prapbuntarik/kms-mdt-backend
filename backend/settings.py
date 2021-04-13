94a268866f07f9d09982cdde4114effa21d759ef
nap3831 <nap3831@gmail.com>
2021-04-13 21:29:34 +0700

    set env

--- backend/settings.py
+++ backend/settings.py
@@ -18,7 +18,8 @@ from pathlib import Path
 env = Env()
 env.read_env()  # read .env file, if it exists
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.abspath(os.path.join(__file__, '../../../'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
