a01eeb6a277cd611938b4fdd3d7d0a9709da4244
nap3831 <nap3831@gmail.com>
2021-04-18 17:52:22 +0700

    Implemented comment services

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .api import BlogAPI, BlogContentViewAPI, BlogContentViewAllAPI, BlogCategoryAPI, BlogDetailAPI
+from .api import BlogAPI, BlogContentViewAPI, BlogContentViewAllAPI, BlogCategoryAPI, BlogDetailAPI, GetBlogCommentViewAPI, BlogCommentAPI
 urlpatterns = [
     path('api/blog', BlogAPI.as_view()),
     path('api/blog/all', BlogContentViewAllAPI.as_view()),
     path('api/blog/get', BlogContentViewAPI.as_view()),
     path('api/blog/<int:pk>', BlogDetailAPI.as_view()),
-    path('api/blog/category', BlogCategoryAPI.as_view())
+    path('api/blog/category', BlogCategoryAPI.as_view()),
+    path('api/blog/comment', BlogCommentAPI.as_view()),
+    path('api/blog/comment/all/<int:pk>', GetBlogCommentViewAPI.as_view())
 ]
